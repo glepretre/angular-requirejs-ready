bd938b5d0985b2f33b7d063aa48f1717dbb5df9a
Gilles Lepretre <gilles.lepretre@lim.eu>
2014-11-10 16:59:26 +0100

    Tidy up karma config

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,24 +1,15 @@
 module.exports = function(config) {
   config.set({
-
-    // base path, that will be used to resolve files and exclude
     basePath: '..',
-
-    // frameworks to use
     frameworks: ['jasmine', 'requirejs'],
-
-    // list of files / patterns to load in the browser
     files: [
-      { pattern: 'src/test-main.js' }, // Must be before 'src/**/*.js' to be included
+      // Must be before 'src/**/*.js' to be included
+      { pattern: 'src/test-main.js' },
       { pattern: 'config/require.conf.js', included: false },
       { pattern: 'bower_components/**/*.js', included: false },
-      { pattern: 'src/**/*.js', included: false }
-//      { pattern: 'src/**/*.html', included: false }
+      { pattern: 'src/**/*.js', included: false },
     ],
-
-    // list of files to exclude
     exclude: [],
-
     preprocessors: {
       /*
        * Do not pre-process html files by default
@@ -26,37 +17,21 @@ module.exports = function(config) {
        */
       '**/*.html': []
     },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
     reporters: ['progress'],
-
-    // web server port
     port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-    // level of logging
-    // possible values:
-    // - config.LOG_DISABLE
-    // - config.LOG_ERROR
-    // - config.LOG_WARN
-    // - config.LOG_INFO
-    // - config.LOG_DEBUG
+    /*
+     * level of logging
+     * possible values:
+     * - config.LOG_DISABLE
+     * - config.LOG_WARN
+     * - config.LOG_INFO
+     * - config.LOG_DEBUG
+     * - config.LOG_ERROR
+     */
     logLevel: config.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
     browsers: [
       'Chrome',
       'Firefox',
@@ -66,8 +41,10 @@ module.exports = function(config) {
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 10000,
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
+    /*
+     * Continuous Integration mode
+     * if true, it capture browsers, run tests and exit
+     */
     singleRun: false
   });
 };
