15dba5d64feed71d84a498bed7731045f21004d8
Gilles Lepretre <gilles.lepretre@lim.eu>
2014-11-12 15:17:44 +0100

    Fix multiline comment style

--- e2e/main.js
+++ e2e/main.js
@@ -1,8 +1,10 @@
 require(['/config/require.conf.js'], function() {
   'use strict';
-  // Angular Bootstrap
-  // (Could not use ng-app with requirejs since
-  // scripts are asynchronously loaded)
+  /*
+   * Angular Bootstrap
+   * Could not use ng-app with requirejs since
+   * scripts are asynchronously loaded
+   */
   require(['angular', '../e2e/e2e'], function(angular) {
     angular.element(document).ready(function() {
       angular.bootstrap(document, ['e2e']);
